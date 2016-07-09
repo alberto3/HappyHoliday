f04663d9b399caf93173ade94de2af2d00552303
Albert Harounian <albert.harounian@gmail.com>
2016-07-09 18:59:55 +0300

    Adding doc, demo video and Arduino sketch
    A POV shows חג שמח (happy holiday).
    An attiny8 is used to control the timing of the LEDs.
    Currently no feedback from the motor - just fixed delays.

---
+++ sketch_apr14a/sketch_apr14a.ino
@@ -0,0 +1,131 @@
+int charBreak = 150;
+
+int LED1 = 0;
+int LED2 = 1;
+int LED3 = 2;
+int LED4 = 3;
+int LED5 = 4;
+int LED6 = 5;
+
+void setup()
+{
+	pinMode(LED1, OUTPUT);
+	pinMode(LED2, OUTPUT);
+	pinMode(LED3, OUTPUT);
+	pinMode(LED4, OUTPUT);
+	pinMode(LED5, OUTPUT);
+	pinMode(LED6, OUTPUT);
+}
+
+void loop()
+{   
+	PORTA = 0b00111111;
+	delayMicroseconds(charBreak);
+	
+	PORTA = 0b00100000;
+	delayMicroseconds(charBreak);
+	
+	PORTA = 0b00100000;
+	delayMicroseconds(charBreak);
+	
+	PORTA = 0b00100000;
+	delayMicroseconds(charBreak);
+	
+	PORTA = 0b00111111;
+	delayMicroseconds(charBreak);
+	
+	PORTA = 0b00000000;  //Open space between characters
+	delayMicroseconds(charBreak);
+	
+	PORTA = 0b00111111;
+	delayMicroseconds(charBreak);
+	
+	PORTA = 0b00100100;
+	delayMicroseconds(charBreak);
+	
+	PORTA = 0b00100100;
+	delayMicroseconds(charBreak);
+	
+	PORTA = 0b00100111;
+	delayMicroseconds(charBreak);
+	
+	PORTA = 0b00000000;
+	delayMicroseconds(charBreak);
+	
+	PORTA = 0b00000000;
+	delayMicroseconds(charBreak*2);  //The word is done so have a bigger space between words
+	
+	PORTA = 0b00111111;
+	delayMicroseconds(charBreak);
+	
+	PORTA = 0b00000001;
+	delayMicroseconds(charBreak);
+	
+	PORTA = 0b00111111;
+	delayMicroseconds(charBreak);
+	
+	PORTA = 0b00000001;
+	delayMicroseconds(charBreak);
+	
+	PORTA = 0b00111111;
+	delayMicroseconds(charBreak);
+	
+	PORTA = 0b00000000;  //Open space between characters
+	delayMicroseconds(charBreak);
+	
+	PORTA = 0b00011111;
+	delayMicroseconds(charBreak);
+	
+	PORTA = 0b00010001;
+	delayMicroseconds(charBreak);
+	
+	PORTA = 0b00010001;
+	delayMicroseconds(charBreak);
+	
+	PORTA = 0b00010000;
+	delayMicroseconds(charBreak);
+	
+	PORTA = 0b00111111;
+	delayMicroseconds(charBreak);
+	
+	PORTA = 0b00000000;  //Open space between characters
+	delayMicroseconds(charBreak);
+	
+	PORTA = 0b00111111;
+	delayMicroseconds(charBreak);
+	
+	PORTA = 0b00100000;
+	delayMicroseconds(charBreak);
+	
+	PORTA = 0b00100000;
+	delayMicroseconds(charBreak);
+	
+	PORTA = 0b00100000;
+	delayMicroseconds(charBreak);
+	
+	PORTA = 0b00111111;
+	delayMicroseconds(charBreak);
+	
+	PORTA = 0b00000000; //The sentence is done so have a HUGE space 
+	delayMicroseconds(600);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
